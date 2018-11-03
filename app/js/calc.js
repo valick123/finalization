1643f02ac013788ebd9ff642414fb38d73a25127
valick123 <valentin3840@gmail.com>
2018-11-04 01:34:03 +0300

    create a calc widget

--- app/js/calc.js
+++ app/js/calc.js
@@ -1,50 +1,99 @@
 "use strict";
-const Widget = document.querySelector(".calc-widget");
-const RoomAmount = document.querySelector(".roomamount");
-const BathRoomAmount = document.querySelector(".bathroomamount");
-const SubmitButton = document.querySelector(".calc-submitbtn");
-let roomAmount = 1;
-let bathRoomAmount = 1;
-let cost = 42.9;
-console.log(RoomAmount.dataset.roomCost.split(",").length);
-const nextRoom = () => {
-  roomAmount++;
 
-  RoomAmount.innerHTML = roomAmount + " rooms";
-  cost += 15;
-  SubmitButton.innerHTML = "Заказать за " + cost.toFixed(2) + " руб";
-};
-const prevRoom = () => {
-  roomAmount--;
-  RoomAmount.innerHTML = roomAmount + " romms";
-  cost -= 15;
-  SubmitButton.innerHTML = "Заказать за " + cost.toFixed(2) + " руб";
-};
-const nextBathRoom = () => {
-  bathRoomAmount++;
-  BathRoomAmount.innerHTML = bathRoomAmount + " batnrooms";
-  fo;
-  cost += 10;
-  SubmitButton.innerHTML = "Заказать за " + cost.toFixed(2) + " руб";
-};
-const prevBathRoom = () => {
-  bathRoomAmount--;
-  BathRoomAmount.innerHTML = bathRoomAmount + " bathrooms";
-  cost -= 10;
-  SubmitButton.innerHTML = "Заказать за " + cost.toFixed(2) + " руб";
-};
-Widget.addEventListener("click", function(e) {
-  let target = e.target;
-  if (target.classList.contains("nextroom")) {
-    nextRoom();
+const rooms = document.querySelector(".rooms");
+const bathrooms = document.querySelector(".bathrooms");
+const result = document.querySelector(".result");
+
+class Calc {
+  constructor(rooms, bathrooms, result) {
+    this.rooms = rooms;
+    this.bathrooms = bathrooms;
+    this.result = result;
+
+    this.roomPrice = this.rooms.dataset.roomPrice.split(",");
+    this.bathRoomPrice = this.bathrooms.dataset.bathroomPrice.split(",");
+
+    this.maxRoom = this.roomPrice.length;
+    this.maxBathRoom = this.bathRoomPrice.length;
+
+    this.roomAmount = +this.rooms.dataset.roomDefult;
+    this.bathRoomAmount = +this.bathrooms.dataset.bathroomDefult;
   }
-  if (target.classList.contains("prevroom")) {
-    prevRoom();
+  RoomText(count) {
+    let text = "";
+    if (count > 1 && count <= this.maxRoom) {
+      text = " комнаты";
+    } else if (count >= this.maxRoom) {
+      text = " комнат";
+    } else {
+      text = " комната";
+    }
+    return text;
   }
-  if (target.classList.contains("nextbathroom")) {
-    nextBathRoom();
+  BathRoomText(count) {
+    let text = "";
+    if (count > 1 && count <= this.maxBathRoom) {
+      text = " санузла";
+    } else if (count >= this.maxBathRoom) {
+      text = " санузлов";
+    } else {
+      text = " санузел";
+    }
+    return text;
   }
-  if (target.classList.contains("prevbathroom")) {
-    prevBathRoom();
+  nextRoom() {
+    this.roomAmount++;
+    roomText.innerHTML = this.roomAmount + this.RoomText(this.roomAmount);
   }
-});
+  prevRoom() {
+    this.roomAmount--;
+    roomText.innerHTML = this.roomAmount + this.RoomText(this.roomAmount);
+  }
+  nextBathRoom() {
+    this.bathRoomAmount++;
+    bathRoomText.innerHTML =
+      this.bathRoomAmount + this.BathRoomText(this.bathRoomAmount);
+  }
+  prevBathRoom() {
+    this.bathRoomAmount--;
+    bathRoomText.innerHTML =
+      this.bathRoomAmount + this.BathRoomText(this.bathRoomAmount);
+  }
+  Result() {
+    let totalPrice =
+      +this.roomPrice[this.roomAmount - 1] +
+      +this.bathRoomPrice[this.bathRoomAmount - 1];
+    this.result.innerHTML = "Заказать за " + totalPrice.toFixed(2) + " руб";
+  }
+  init() {
+    let self = this;
+    this.rooms.addEventListener("click", function(e) {
+      let target = e.target;
+      if (
+        target.classList.contains("next") &&
+        self.roomAmount != self.maxRoom
+      ) {
+        self.nextRoom();
+      }
+      if (target.classList.contains("prev") && self.roomAmount != 1) {
+        self.prevRoom();
+      }
+      self.Result();
+    });
+    this.bathrooms.addEventListener("click", function(e) {
+      let target = e.target;
+      if (
+        target.classList.contains("next") &&
+        self.bathRoomAmount != self.maxBathRoom
+      ) {
+        self.nextBathRoom();
+      }
+      if (target.classList.contains("prev") && self.bathRoomAmount != 1) {
+        self.prevBathRoom();
+      }
+      self.Result();
+    });
+  }
+}
+let calc = new Calc(rooms, bathrooms, result);
+calc.init();
